7fe9937787e066b631d992c3432cb55163075347
Matthew Walker <mwalker@kydance.net>
2010-11-04 16:21:08 -0600

    Major cleanup
    Re-formated code to use the same standard throughout
    Moved class files into correct subfolder based on package

---
+++ Tim/FridgeItem.java
@@ -0,0 +1,27 @@
+package Tim;
+
+import java.io.Serializable;
+
+public class FridgeItem implements Serializable {
+
+    private static final long serialVersionUID = -1658316674659907588L;
+    private String name;
+    private boolean approved;
+
+    public FridgeItem(String str, boolean flag) {
+        this.name = str.toLowerCase();
+        this.approved = flag;
+    }
+
+    public boolean equals(FridgeItem item) {
+        return this.name.equalsIgnoreCase(item.name);
+    }
+
+    public boolean equals(String itemname) {
+        return this.name.equalsIgnoreCase(itemname);
+    }
+
+    public boolean isApproved() {
+        return this.approved;
+    }
+}
