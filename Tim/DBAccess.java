1ae6b28389dad1129173abb4be7fb42c33887025
Matthew Walker <mwalker@kydance.net>
2012-09-30 19:59:15 -0600

    Starting port to PircBotX
    Porting to PircBotX to enable support for UTF-8, as well as other major
    upgrades. This will be a multi-week process, but is moving along well
    already.

---
+++ Tim/DBAccess.java
@@ -0,0 +1,91 @@
+/**
+ * This file is part of Timmy, the Wordwar Bot.
+ *
+ * Timmy is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * Timmy is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Timmy. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+package Tim;
+
+import com.mysql.jdbc.Driver;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import snaq.db.ConnectionPool;
+
+/**
+ *
+ * @author Matthew Walker
+ */
+public class DBAccess {
+	private static DBAccess instance;
+
+	private long timeout = 3000;
+	
+	protected ConnectionPool pool;
+
+	static {
+        instance = new DBAccess();
+    }
+
+	private DBAccess() {
+		Class c;
+		Driver driver;
+
+		/**
+		 * Make sure the JDBC driver is initialized. Used by the connection pool.
+		 */
+		try {
+			c = Class.forName("com.mysql.jdbc.Driver");
+			driver = (Driver) c.newInstance();
+			DriverManager.registerDriver(driver);
+		} catch (Exception ex) {
+			Logger.getLogger(Tim.class.getName()).log(Level.SEVERE, null, ex);
+		}
+
+		// Initialize the connection pool, to prevent SQL timeout issues
+		String url = "jdbc:mysql://" + Tim.config.getString("sql_server") + ":3306/" + Tim.config.getString("sql_database");
+		pool = new ConnectionPool("local", 5, 25, 50, 180000, url, Tim.config.getString("sql_user"), Tim.config.getString("sql_password"));
+	}
+
+	/**
+     * Singleton access method.
+     *
+     * @return Singleton
+     */
+    public static DBAccess getInstance() {
+		return instance;
+    }
+
+	public String getSetting( String key ) {
+		Connection con;
+		String value = "";
+
+		try {
+			con = pool.getConnection(timeout);
+
+			PreparedStatement s = con.prepareStatement("SELECT `value` FROM `settings` WHERE `key` = ?");
+			s.setString(1, key);
+			s.executeQuery();
+
+			ResultSet rs = s.getResultSet();
+			while (rs.next()) {
+				value = rs.getString("value");
+			}
+
+			con.close();
+		} catch (Exception ex) {
+			Logger.getLogger(Tim.class.getName()).log(Level.SEVERE, null, ex);
+		}
+
+		return value;
+	}
+}
