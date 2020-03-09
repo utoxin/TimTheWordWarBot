42d63a5ba9552d15e9a2ec18ea1929db5131d7ae
Matthew Walker <utoxin@gmail.com>
2020-03-08 23:26:49 -0600

    Working on raptor commands

--- timmy/command_processors/amusement/raptorcommands.py
+++ timmy/command_processors/amusement/raptorcommands.py
@@ -1,14 +1,99 @@
+from timmy import db_access
 from timmy.command_processors.base_command import BaseCommand
 from timmy.data.command_data import CommandData
 
 
 class RaptorCommands(BaseCommand):
-    user_commands = {"raptor", "testwhois"}
+    user_commands = {"raptor"}
     sub_commands = {"adopt", "release", "details", "rename"}
 
     def process(self, connection, event, command_data: CommandData):
-        if command_data.command == "testwhois":
-            connection.whois(command_data.issuer)
+        user_data = db_access.user_directory.nick_directory.get(command_data.issuer)
+
+        if user_data is None or not user_data.registration_data_retrieved:
+            self.respond_to_user(connection, event, "I'm sorry, you must be registered before you can work with "
+                                                    "raptors... (Please ensure you are registered and identified with "
+                                                    "NickServ.)")
+            return
+
+        if command_data.arg_count <= 1:
+            self.respond_to_user(connection, event, "Raptors recently became fascinated by writing, so we created a "
+                                                    "raptor adoption program. They will monitor your word sprints, and "
+                                                    "share the details with you when asked. Don't worry, they probably "
+                                                    "won't eat your cat.")
+            self.respond_to_user(connection, event, "Valid subcommands: adopt, release, details, rename")
             return
 
         self.handle_subcommand(connection, event, command_data)
+
+    def _adopt_handler(self, connection, event, command_data: CommandData):
+        user_data = db_access.user_directory.nick_directory.get(command_data.issuer)
+
+        if user_data.raptor_adopted:
+            self.respond_to_user(connection, event, "Due to safety regulations, you may only adopt a single raptor.")
+            return
+
+        if command_data.arg_count < 2:
+            self.respond_to_user(connection, event, "You need to provide a name to adopt your new raptor.")
+            return
+
+        user_data.raptor_adopted = True
+        user_data.raptor_name = command_data.args[1:]
+        # TODO: Set raptor's favorite color using lists
+
+        user_data.save()
+
+        self.respond_to_user(connection, event, "Excellent! I've noted your raptor's name. Just so you know, their "
+                                                "favorite color is " + user_data.raptor_favorite_color + ".")
+
+        return
+
+    def _release_handler(self, connection, event, command_data: CommandData):
+        user_data = db_access.user_directory.nick_directory.get(command_data.issuer)
+
+        if not user_data.raptor_adopted:
+            self.respond_to_user(connection, event, "I don't have any record of you having a raptor. Are you sure that "
+                                                    "isn't your cat?")
+
+            return
+
+        self.respond_to_user(connection, event, "I'll release " + user_data.raptor_name + " back into the wild. I'm "
+                                                                                          "sure they'll adjust well...")
+
+        user_data.raptor_adopted = False
+        user_data.total_sprint_wordcount = 0
+        user_data.total_sprints = 0
+        user_data.total_sprint_duration = 0
+        user_data.raptor_adopted = False
+        user_data.raptor_name = ""
+        user_data.raptor_favorite_color = ""
+        user_data.raptor_bunnies_stolen = 0
+        user_data.last_bunny_raid = None
+        user_data.recorded_wars = {}
+
+        user_data.save()
+
+        return
+
+    def _details_handler(self, connection, event, command_data: CommandData):
+        
+
+    def _rename_handler(self, connection, event, command_data: CommandData):
+        user_data = db_access.user_directory.nick_directory.get(command_data.issuer)
+
+        if not user_data.raptor_adopted:
+            self.respond_to_user(connection, event, "I don't have any record of you having a raptor. I can't rename "
+                                                    "your children.")
+            return
+
+        if command_data.arg_count < 2:
+            self.respond_to_user(connection, event, "You need to provide a new name.")
+            return
+
+        user_data.raptor_name = command_data.args[1:]
+        user_data.save()
+
+        self.respond_to_user(connection, event, "Okay... your raptor should respond to the name " +
+                             command_data.args[1:] + " now. Don't do this too often or You'll confuse the poor thing.")
+
+        return
--- timmy/core/userperms.py
+++ timmy/core/userperms.py
@@ -1,14 +1,36 @@
+from irc.dict import IRCDict
+
 from timmy.core import bot_instance
+from timmy import db_access
 
 
 class UserPerms:
-    @staticmethod
-    def is_admin(user, channel):
+    def __init__(self):
+        self.admin_data_loaded = False
+        self.admins = IRCDict()
+
+    def _load_admin_data(self):
+        select_statement = "SELECT * FROM `admins`"
+
+        connection = db_access.connection_pool.get_connection()
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute(select_statement)
+
+        for row in cursor:
+            self.admins[row['name']] = True
+
+        self.admin_data_loaded = True
+
+    def is_admin(self, user, channel):
         if user in bot_instance.channels[channel].opers():
             return True
 
-        return False
+        if not self.admin_data_loaded:
+            self._load_admin_data()
+
+        return user in self.admins or channel in self.admins
 
     @staticmethod
     def is_registered(user):
-        return False
+        user_data = db_access.user_directory.nick_directory.get(user)
+        return user_data.registration_data_retrieved if user_data is not None else False
