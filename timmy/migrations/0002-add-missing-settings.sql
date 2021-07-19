52b8d36c043e6c6e94fed3640d59ecfdcead5f8f
Matthew Walker <utoxin@gmail.com>
2021-07-19 07:23:11 -0600

    Working on migrations

--- timmy/migrations/0002-add-missing-settings.sql
+++ timmy/migrations/0002-add-missing-settings.sql
@@ -2,3 +2,11 @@
 -- file: 0002-migrations/add-missing-settings.sql
 --
 
+INSERT INTO
+    settings (`key`, `value`)
+    VALUES
+           ('nickname', 'Timmy'),
+           ('realname', 'Timmy'),
+           ('server', 'timmy-irc'),
+           ('do_irc_logging', 1),
+           ('irc_logging_channel', '#timmy-debug');
