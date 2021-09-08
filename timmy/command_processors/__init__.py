c529e7f85b1cf6d9ee3f39de36289edf94c67621
Matthew Walker <utoxin@gmail.com>
2021-09-07 21:45:46 -0600

    TimTheWordWarBot-23 Migrate !challenge and !challengefor Commands

--- timmy/command_processors/__init__.py
+++ timmy/command_processors/__init__.py
@@ -12,6 +12,7 @@ from .amusement.getcommands import GetCommands
 from .amusement.herdcommand import HerdCommand
 from .amusement.lickcommand import LickCommand
 from .amusement.raptorcommands import RaptorCommands
+from .amusement.search import SearchCommand
 from .amusement.woot import WootCommand
 from .utility.admincommands import AdminCommands
 from .utility.channelcommands import ChannelCommands
@@ -94,3 +95,6 @@ def register_processors():
 
     challengecommand = ChallengeCommands()
     challengecommand.register_commands(event_handlers.command_handler_instance)
+
+    search = SearchCommand()
+    search.register_commands(event_handlers.command_handler_instance)
